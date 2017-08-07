d4bd305440074de766501380cb6df8cf520b795e
kwiatkowskaela <kwiatkowska.grafik@gmail.com>
2017-08-07 17:07:12 +0200

    zmiana return już działa

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,12 @@ function getTriangleArea(a, h) {
         console.log("Nieprawidłowe dane");
         return;
     } else {
-        return a * h / 2;
+
         alert('Pole trójkąta, ' + a * h / 2 + 'cm2');
         console.log('Pole trójkąta, ' + triangleArea);
+        return a * h / 2;
     }
 }
 
 var suma = getTriangleArea(prompt("wpisz długość boku trójkąta"), prompt("wpisz wysokość trójkąta"));
 console.log(suma);
-
-
-var triangleArea = getTriangleArea prompt(a, h);
-console.log('Pole trójkąta, ' + triangleArea);
